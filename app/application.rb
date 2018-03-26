f52e34093b7dadcdd1f2315f7a506e7789d7ab73
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-26 01:58:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,13 +5,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new
 
- if req.path.match(/items\/<ITEM NAME>/)
-   binding.pry
-  @match = @@items.detect{|item| item = req.path.last }
-   resp.write price
-else
-  resp.status = 404
+binding.pry
 end
 
-  end
+
 end
