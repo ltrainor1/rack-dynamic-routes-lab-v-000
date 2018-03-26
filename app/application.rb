d517ebcc5b7b5a07de5086afe7a02d22c493c863
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-26 01:58:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,12 +4,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new
-binding.pry
+
  if req.path.match(/items\/<ITEM NAME>/)
    binding.pry
   @match = @@items.detect{|item| item = req.path.last }
    resp.write price
-   binding.pry
 else
   resp.status = 404
 end
